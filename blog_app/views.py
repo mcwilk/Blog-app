3311656a7431363af4566bc97b8e889c6bc45460
mcwilk <maciej.wilk04@gmail.com>
2020-04-18 02:07:30 +0200

    templates and static added

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-#def index(request):
+def index(request):
+    post_list = Post.objects.filter(publication_date__lte=timezone.now()).order_by('-publication_date')[:5]
+    last_post_id = post_list[0].id
+    return render(request, 'blog_app/index.html', {'post_list': post_list, 'last_post_id': last_post_id})
+
+
 
