cf4074fbfdf9df58a679edf699e09325fe7f6d07
mcwilk <maciej.wilk04@gmail.com>
2020-04-20 17:54:06 +0200

    index template modification

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,9 +3,9 @@ from django.utils import timezone
 from .models import Post
 
 def index(request):
-    post_list = Post.objects.filter(publication_date__lte=timezone.now()).order_by('-publication_date')[:5]
-    last_post_id = post_list[0].id
-    return render(request, 'blog_app/index.html', {'post_list': post_list, 'last_post_id': last_post_id})
+    post_list = Post.objects.filter(publication_date__lte=timezone.now()).order_by('-publication_date')[:3]
+    unique_years = sorted([d.year for d in Post.objects.all().datetimes('publication_date', 'year')], reverse=True)
+    return render(request, 'blog_app/index.html', {'post_list': post_list, 'unique_years': unique_years})
 
 
 
